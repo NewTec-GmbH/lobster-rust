887aebbbd5ec033ce948ccd7b7f92cbd72dd0a61
Luca <luca.dubies@newtec.de>
2025-01-24 12:01:24 +0100

    Reworing impl_data to context_data to allow handling of local modules.

--- src/traceable_node.rs
+++ src/traceable_node.rs
@@ -1,12 +1,10 @@
+use json::{object::Object, JsonValue};
+use ra_ap_syntax::{SyntaxKind, SyntaxNode};
 /// RustTraceableNode to hold information from parsing the syntax tree.
 /// Also holds auxiliary structs and functions.
-
 use std::fmt::Display;
-use ra_ap_syntax::{SyntaxNode, SyntaxKind};
-use json::{object::Object, JsonValue};
 
-use crate::{NodeLocation, FileReference, syntax_extensions::Searchable};
-
+use crate::{syntax_extensions::Searchable, FileReference, NodeLocation};
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub(crate) enum NodeKind {
@@ -15,18 +13,18 @@ pub(crate) enum NodeKind {
     Enum,
     Trait,
     Function,
-    Impl
+    Context,
 }
 
 impl NodeKind {
-    pub(crate) fn to_string(&self) -> &str{
+    pub(crate) fn to_string(&self) -> &str {
         match self {
             NodeKind::Module => "Module",
             NodeKind::Struct => "Struct",
             NodeKind::Enum => "Enum",
             NodeKind::Trait => "Trait",
             NodeKind::Function => "Function",
-            NodeKind::Impl => "Impl"
+            NodeKind::Context => "Context",
         }
     }
 }
@@ -40,7 +38,7 @@ pub(crate) struct RustTraceableNode {
     pub(crate) children: Vec<RustTraceableNode>,
     pub(crate) just: Vec<String>,
     pub(crate) refs: Vec<String>,
-    pub(crate) impl_data: Option<ImplementationData>
+    pub(crate) context_data: Option<ContextData>,
 }
 
 impl RustTraceableNode {
@@ -52,7 +50,7 @@ impl RustTraceableNode {
             children: Vec::new(),
             just: Vec::new(),
             refs: Vec::new(),
-            impl_data: None
+            context_data: None,
         }
     }
 
@@ -60,71 +58,108 @@ impl RustTraceableNode {
         let location = FileReference {
             filename: "main.rs".to_string(),
             line: None,
-            column: None
+            column: None,
         };
-        
+
         // lobster-trace: PARSE.nodes
         if let Some(node_kind) = syntax_kind_to_node_kind(node.kind()) {
             match node_kind {
                 NodeKind::Function => {
                     let name_node = node.get_child_kind(SyntaxKind::NAME)?;
-                    let name =  prefix + "." + &name_node.text().to_string();
-                    Some(RustTraceableNode::new(name, NodeLocation::File(location), node_kind))
-                },
-                NodeKind::Module => {
-                    Some(RustTraceableNode::new("FILE".to_string(), NodeLocation::File(location), node_kind))
-                },
+                    let name = prefix + "." + &name_node.text().to_string();
+                    Some(RustTraceableNode::new(
+                        name,
+                        NodeLocation::File(location),
+                        node_kind,
+                    ))
+                }
+                NodeKind::Module => Some(RustTraceableNode::new(
+                    "FILE".to_string(),
+                    NodeLocation::File(location),
+                    node_kind,
+                )),
                 NodeKind::Struct => {
                     let name_node = node.get_child_kind(SyntaxKind::NAME)?;
-                    let name = prefix+ "."  + &name_node.text().to_string();
-                    Some(RustTraceableNode::new(name, NodeLocation::File(location), node_kind))
-                },
-                NodeKind::Impl => {
-                    let traceable_node: Option<RustTraceableNode>;
-                    // get target and optional trait that gets implemented
-                    let path_nodes = node.get_children_kind(SyntaxKind::PATH_TYPE);
-
-                    // either impl STRUCTNAME or impl TRAITNAME for STRUCTNAME
-                    if path_nodes.len() == 2 {
-                        // expect the for kw to be present
-                        let for_kw = node.get_tokens_kind(SyntaxKind::FOR_KW);
-                        if for_kw.len() == 0 {
-                            traceable_node = None;
-                        } else {
-                            let traitref = path_nodes[0].text().to_string();
-                            let structref = path_nodes[1].text().to_string();
-                            let impl_data = ImplementationData::new(structref, Some(traitref));
-                            let mut new_node = RustTraceableNode::new("Impl".to_string(), NodeLocation::File(location), node_kind);
-                            new_node.impl_data = Some(impl_data);
-                            traceable_node = Some(new_node);
-                        }
-                    } else if path_nodes.len() == 1 {
-                        let structref = path_nodes[0].text().to_string();
-                        let impl_data = ImplementationData::new(structref, None);
-                        let mut new_node = RustTraceableNode::new("Impl".to_string(), NodeLocation::File(location), node_kind);
-                        new_node.impl_data = Some(impl_data);
-                        traceable_node = Some(new_node);
-                    } else {
-                        println!("No path nodes in impl???");
-                        traceable_node =  None;
-                    }
-                    traceable_node
+                    let name = prefix + "." + &name_node.text().to_string();
+                    Some(RustTraceableNode::new(
+                        name,
+                        NodeLocation::File(location),
+                        node_kind,
+                    ))
                 }
-                _ => None
+                NodeKind::Context => match node.kind() {
+                    SyntaxKind::IMPL => RustTraceableNode::from_impl_node(node, location),
+                    SyntaxKind::MODULE => RustTraceableNode::from_module_node(node, location),
+                    _ => None,
+                },
+                _ => None,
             }
-
         } else {
             None
         }
     }
 
-    pub(crate) fn from_node_with_location(node: &SyntaxNode, location: NodeLocation, prefix: String) -> Option<Self> {
+    fn from_impl_node(node: &SyntaxNode, location: FileReference) -> Option<Self> {
+        // get target and optional trait that gets implemented
+        let path_nodes = node.get_children_kind(SyntaxKind::PATH_TYPE);
+
+        // either impl STRUCTNAME or impl TRAITNAME for STRUCTNAME
+        if path_nodes.len() == 2 {
+            // expect the for kw to be present
+            let for_kw = node.get_tokens_kind(SyntaxKind::FOR_KW);
+            if for_kw.len() == 0 {
+                None
+            } else {
+                let traitref = path_nodes[0].text().to_string();
+                let structref = path_nodes[1].text().to_string();
+                let impl_data = ContextData::new(structref, Some(traitref));
+                let mut new_node = RustTraceableNode::new(
+                    "Impl".to_string(),
+                    NodeLocation::File(location),
+                    NodeKind::Context,
+                );
+                new_node.context_data = Some(impl_data);
+                Some(new_node)
+            }
+        } else if path_nodes.len() == 1 {
+            let structref = path_nodes[0].text().to_string();
+            let impl_data = ContextData::new(structref, None);
+            let mut new_node = RustTraceableNode::new(
+                "Impl".to_string(),
+                NodeLocation::File(location),
+                NodeKind::Context,
+            );
+            new_node.context_data = Some(impl_data);
+            Some(new_node)
+        } else {
+            println!("No path nodes in impl???");
+            None
+        }
+    }
+
+    fn from_module_node(node: &SyntaxNode, location: FileReference) -> Option<Self> {
+        let name_node = node.get_child_kind(SyntaxKind::NAME).unwrap();
+        let mut new_node = RustTraceableNode::new(
+            name_node.text().to_string(),
+            NodeLocation::File(location),
+            NodeKind::Context,
+        );
+        let context = ContextData::new(name_node.text().to_string(), None);
+        new_node.context_data = Some(context);
+        Some(new_node)
+    }
+
+    pub(crate) fn from_node_with_location(
+        node: &SyntaxNode,
+        location: NodeLocation,
+        prefix: String,
+    ) -> Option<Self> {
         let mut new_node = Self::from_node(node, prefix);
-        new_node.as_mut().map(| n| n.location = location);
+        new_node.as_mut().map(|n| n.location = location);
         new_node
     }
 
-    pub(crate) fn append_child(&mut self, child: RustTraceableNode) { 
+    pub(crate) fn append_child(&mut self, child: RustTraceableNode) {
         self.children.push(child);
     }
 
@@ -134,14 +169,14 @@ impl RustTraceableNode {
                 for child in &self.children {
                     child.to_lobster(items);
                 }
-            },
+            }
             NodeKind::Function => {
                 items.push(JsonValue::from(self));
-            },
+            }
             NodeKind::Struct => {
                 items.push(JsonValue::from(self));
-            },
-            NodeKind::Impl => {
+            }
+            NodeKind::Context => {
                 for child in &self.children {
                     child.to_lobster(items);
                 }
@@ -153,7 +188,13 @@ impl RustTraceableNode {
 
 impl Display for RustTraceableNode {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        writeln!(f, "Node {:?} {} at {}", self.kind, self.name, self.location.to_string())?;
+        writeln!(
+            f,
+            "Node {:?} {} at {}",
+            self.kind,
+            self.name,
+            self.location.to_string()
+        )?;
         Ok(())
     }
 }
@@ -166,10 +207,26 @@ impl From<&RustTraceableNode> for JsonValue {
         let _ = json_out.insert("name", format!("{}", node.name));
         let _ = json_out.insert("location", JsonValue::from(&node.location));
         let _ = json_out.insert("messages", JsonValue::Array(Vec::new()));
-        let _ = json_out.insert("just_up", JsonValue::Array(node.just.iter().map(|j| JsonValue::String(j.to_string())).collect()));
+        let _ = json_out.insert(
+            "just_up",
+            JsonValue::Array(
+                node.just
+                    .iter()
+                    .map(|j| JsonValue::String(j.to_string()))
+                    .collect(),
+            ),
+        );
         let _ = json_out.insert("just_down", JsonValue::Array(Vec::new()));
         let _ = json_out.insert("just_global", JsonValue::Array(Vec::new()));
-        let _ = json_out.insert("refs", JsonValue::Array(node.refs.iter().map(|r| JsonValue::String(r.to_string())).collect()));
+        let _ = json_out.insert(
+            "refs",
+            JsonValue::Array(
+                node.refs
+                    .iter()
+                    .map(|r| JsonValue::String(r.to_string()))
+                    .collect(),
+            ),
+        );
         let _ = json_out.insert("language", "Rust");
         let _ = json_out.insert("kind", node.kind.to_string());
         json_out
@@ -177,16 +234,16 @@ impl From<&RustTraceableNode> for JsonValue {
 }
 
 #[derive(Debug, Clone)]
-pub(crate) struct ImplementationData {
-    pub(crate) target: String,
-    pub(crate) _trait_imp: Option<String>
+pub(crate) struct ContextData {
+    pub(crate) namespace: String,
+    pub(crate) _trait_imp: Option<String>,
 }
 
-impl ImplementationData {
-    fn new(target: String, trait_imp: Option<String>) -> Self {
-        ImplementationData {
-            target,
-            _trait_imp: trait_imp
+impl ContextData {
+    fn new(namespace: String, trait_imp: Option<String>) -> Self {
+        ContextData {
+            namespace,
+            _trait_imp: trait_imp,
         }
     }
 }
@@ -198,7 +255,8 @@ fn syntax_kind_to_node_kind(kind: SyntaxKind) -> Option<NodeKind> {
         SyntaxKind::STRUCT => Some(NodeKind::Struct),
         SyntaxKind::ENUM => Some(NodeKind::Enum),
         SyntaxKind::TRAIT => Some(NodeKind::Trait),
-        SyntaxKind::IMPL => Some(NodeKind::Impl),
-        _ => None
+        SyntaxKind::IMPL => Some(NodeKind::Context),
+        SyntaxKind::MODULE => Some(NodeKind::Context),
+        _ => None,
     }
 }
