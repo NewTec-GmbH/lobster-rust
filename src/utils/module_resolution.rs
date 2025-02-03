8f8fa37668d46fc8bc774ec8b710ef91f2517d19
Luca <luca.dubies@newtec.de>
2025-02-03 11:03:10 +0100

    Rework module resolution according to review findings.

--- src/utils/module_resolution.rs
+++ src/utils/module_resolution.rs
@@ -31,137 +31,170 @@
 
 use crate::utils::context::Context;
 use std::fs::{self, DirEntry};
+use std::ops::Deref;
 use std::path::{Path, PathBuf};
 
 /// Resolved a module declaration to a path.
 ///
 /// Tries to resolve a module declaration.
 /// This is dependent on the current file name.
-/// Resolution options are detailed in the code and in [the documentation](https://github.com/NewTec-GmbH/lobster-rust/blob/feature/multifile/doc/module_resolution.md).
+/// Resolution options are detailed in the code and in [the documentation](https://github.com/NewTec-GmbH/lobster-rust/blob/main/doc/module_resolution.md).
 /// Additionally builds a context string if the module could be resolved to a path.
 ///
 /// ### Parameters
 /// * `current_file` - Path to the current file (where the module was declared via the ```mod``` keyword).
-/// * `target_name` - Module name (The module name specified after the ```mod``` keyword).
+/// * `target_module_name` - Module name (The module name specified after the ```mod``` keyword).
 ///
 /// ### Returns
 /// Some(PathBuf, Context) if the module could be resolved to a path.
 ///
 pub(crate) fn resolve_module_declaration(
     current_file: &Path,
-    target_name: &str,
+    target_module_name: &str,
 ) -> Option<(PathBuf, Context)> {
     // Get cwd and target file name.
     let current_path = current_file.parent()?;
     let current_file_stem = current_file.file_stem()?.to_str()?;
-    let file_target = target_name.to_string() + ".rs";
+    let file_target = target_module_name.to_string() + ".rs";
 
     // Read cwd contents.
     let directory_read_results = fs::read_dir(current_path).ok()?;
-    let directory_content: Vec<DirEntry> = directory_read_results
-        .filter_map(|entry_result| entry_result.ok())
+    let directory_content: Vec<PathBuf> = directory_read_results
+        .filter_map(|entry_result| entry_result.ok().map(|content| content.path()))
         .collect();
 
-    // Option 1 and 2 only available for main.rs, lib.rs or mod.rs.
+    // For main.rs, lib.rs or mod.rs,
+    // Rust tries to resolve the module in the current directory.
     if ["main", "lib", "mod"].contains(&current_file_stem) {
-        // Option 1: target.rs file in the same directory.
-        for directory_entry in &directory_content {
-            if let Some(file_name) = directory_entry.path().file_name() {
-                if &file_target == file_name.to_str().unwrap() {
-                    // Return path to target_name.rs
-                    return Some((directory_entry.path(), Context::Empty));
-                }
-            }
-        }
-
-        // Option 2: target directory with mod.rs.
-        for directory_entry in &directory_content {
-            if (directory_entry.path().is_dir()) && (directory_entry.path().ends_with(target_name))
-            {
-                let subdirectory_content: Vec<DirEntry> = fs::read_dir(directory_entry.path())
-                    .ok()?
-                    .filter_map(|entry_result| entry_result.ok())
-                    .collect();
-                // If a subdirectory with the target name exists, find mod.rs in it.
-                for subdirectory_entry in subdirectory_content {
-                    if let Some(file_name) = subdirectory_entry.path().file_name() {
-                        if "mod.rs" == file_name.to_str().unwrap() {
-                            // Return path to mod.rs.
-                            return Some((
-                                subdirectory_entry.path(),
-                                Context::from_str(target_name),
-                            ));
-                        }
-                    }
-                }
-            }
+        // Option 1: file named target.rs
+        if let Some(file_result) = check_file_module(&directory_content, &file_target) {
+            return Some(file_result);
+        } else {
+            // Option 2: target directory with mod.rs.
+            check_directory_module(&directory_content, &target_module_name)
         }
     } else {
-        // Option 3 and 4 only for files that are not main.rs, lib.rs or mod.rs.
+        // For files other than main.rs, lib.rs or mod.rs,
+        // Rust tries to resolve the submodule in a subdirectory with the same name as the current file.
+        check_nested_submodule(
+            &directory_content,
+            &file_target,
+            &target_module_name,
+            &current_file_stem,
+        )
+    }
+}
 
-        // Option 3: target.rs in directory with same name as current file.
-        for directory_entry in &directory_content {
-            if (directory_entry.path().is_dir())
-                && (directory_entry.path().ends_with(current_file_stem))
-            {
-                let subdirectory_content: Vec<DirEntry> = fs::read_dir(directory_entry.path())
-                    .ok()?
-                    .filter_map(|entry_result| entry_result.ok())
-                    .collect();
-                // Find the target file in the subdirectory with the same name as the current file.
-                for subdirectory_entry in subdirectory_content {
-                    if let Some(file_name) = subdirectory_entry.path().file_name() {
-                        if file_target == file_name.to_str().unwrap() {
-                            // Return path to nested target_name.rs
-                            return Some((
-                                subdirectory_entry.path(),
-                                Context::from_str(current_file_stem),
-                            ));
-                        }
-                    }
-                }
+/// Check for a file in the given directory contents with the module name.
+///
+/// Searches the provided directory contents for a rust source file that would match the module name.
+/// If the file is found, a PathBuf to the found file and an empty Context are returned.
+/// The Context is empty, as the file lies in the same directory and therefore the same Context.
+///
+/// ### Parameters
+/// * `directory_content` - Paths to contents of the current directory.
+/// * `file_target` - File name to search for to resolve the module.
+///
+/// ### Returns
+/// Some(PathBuf, Context) if the module could be resolved to a file.
+fn check_file_module(
+    directory_content: &Vec<PathBuf>,
+    file_target: &str,
+) -> Option<(PathBuf, Context)> {
+    for directory_entry in directory_content {
+        if let Some(file_name) = directory_entry.file_name() {
+            if file_target == file_name.to_str().unwrap() {
+                // Return path to target_name.rs
+                return Some((directory_entry.deref().to_path_buf(), Context::Empty));
             }
         }
+    }
+    None
+}
 
-        // Option 4: target/mod.rs in directory with the same name as the current file.
-        let current_file_stem = current_file.file_stem()?.to_str()?;
-        for directory_entry in &directory_content {
-            if (directory_entry.path().is_dir())
-                && (directory_entry.path().ends_with(current_file_stem))
-            {
-                let subdirectory_content: Vec<DirEntry> = fs::read_dir(directory_entry.path())
-                    .ok()?
-                    .filter_map(|entry_result| entry_result.ok())
-                    .collect();
-                // Find the target directory with a mod.rs in the subdirectory with the same name as the current file.
-                for subdirectory_entry in subdirectory_content {
-                    if (subdirectory_entry.path().is_dir())
-                        && (subdirectory_entry.path().ends_with(target_name))
-                    {
-                        // Search for mod.rs in nested directory.
-                        let subsubdirectory_content: Vec<DirEntry> =
-                            fs::read_dir(subdirectory_entry.path())
-                                .ok()?
-                                .filter_map(|entry_result| entry_result.ok())
-                                .collect();
-                        for subsubdirectory_entry in subsubdirectory_content {
-                            if let Some(file_name) = subdirectory_entry.path().file_name() {
-                                if "mod.rs" == file_name.to_str().unwrap() {
-                                    // Return path to nested mod.rs
-                                    return Some((
-                                        subsubdirectory_entry.path(),
-                                        Context::from_str(current_file_stem)
-                                            + Context::from_str(target_name),
-                                    ));
-                                }
-                            }
-                        }
+/// Check for a directory in the given directory contents with the module name that contains a mod.rs.
+///
+/// Searches the provided directory contents for a directory that matches the module name.
+/// If the directory is found, it is checked to contain a mod.rs.
+/// If a mod.rs is found, the PathBuf to the mod.rs is returned with a Context.
+/// The Context contains the directory name to reflect the additional nesting.
+///
+/// ### Parameters
+/// * `directory_content` - Paths to contents of the current directory.
+/// * `target_module_name` - Directory name to search for to resolve the module.
+///
+/// ### Returns
+/// Some(PathBuf, Context) if the module could be resolved to a directory (with mod.rs).
+fn check_directory_module(
+    directory_content: &Vec<PathBuf>,
+    target_module_name: &str,
+) -> Option<(PathBuf, Context)> {
+    for directory_entry in directory_content {
+        if (directory_entry.is_dir()) && (directory_entry.ends_with(target_module_name)) {
+            let subdirectory_content: Vec<DirEntry> = fs::read_dir(directory_entry)
+                .ok()?
+                .filter_map(|entry_result| entry_result.ok())
+                .collect();
+            // If a subdirectory with the target name exists, find mod.rs in it.
+            for subdirectory_entry in subdirectory_content {
+                if let Some(file_name) = subdirectory_entry.path().file_name() {
+                    if "mod.rs" == file_name.to_str().unwrap() {
+                        // Return path to mod.rs.
+                        return Some((
+                            subdirectory_entry.path(),
+                            Context::from_str(target_module_name),
+                        ));
                     }
                 }
             }
         }
     }
+    None
+}
 
-    // Include could not be resolved.
+/// Resolve a submodule.
+///
+/// Searches the provided directory contents for a subdirectory that matches the current file name.
+/// This subdirectory is then searched for the target submodule, either as a file module or a directory module.
+///
+/// ### Parameters
+/// * `directory_content` - Paths to contents of the current directory.
+/// * `file_target` - File name to search for to resolve the module.
+/// * `target_module_name` - Directory name to search for to resolve the module.
+///
+/// ### Returns
+/// Some(PathBuf, Context) if the module could be resolved to a source file or directory (with mod.rs).
+fn check_nested_submodule(
+    directory_content: &Vec<PathBuf>,
+    file_target: &str,
+    target_module_name: &str,
+    current_file_stem: &str,
+) -> Option<(PathBuf, Context)> {
+    // Find subdirectory with the same name as the current file.
+    if let Some(subdirectory) = directory_content
+        .iter()
+        .filter(|directory_entry| {
+            directory_entry.is_dir() && directory_entry.ends_with(current_file_stem)
+        })
+        .next()
+    {
+        // Get the contents of the subdirectory.
+        let subdirectory_content: Vec<PathBuf> = fs::read_dir(subdirectory)
+            .ok()?
+            .filter_map(|entry_result| entry_result.ok().map(|content| content.path()))
+            .collect();
+        let subdirectory_context = Context::from_str(current_file_stem);
+
+        // Try to resolve the submodule to a file or directory in the subdirectory.
+        if let Some((file_module_path, nested_context)) =
+            check_file_module(&subdirectory_content, file_target)
+        {
+            return Some((file_module_path, subdirectory_context + nested_context));
+        } else if let Some((directory_module_path, nested_context)) =
+            check_directory_module(&subdirectory_content, target_module_name)
+        {
+            return Some((directory_module_path, subdirectory_context + nested_context));
+        }
+    }
     None
 }
