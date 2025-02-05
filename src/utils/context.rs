9679cb6dd9b9f1f4080b1a018a5aad09d4d5f486
Luca <luca.dubies@newtec.de>
2025-02-05 10:59:23 +0100

    Typo fix.

--- src/utils/context.rs
+++ src/utils/context.rs
@@ -27,7 +27,7 @@
 // OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-//! Context module to efficiently track namespaces thoruhg module and file nestings.
+//! Context module to efficiently track namespaces through module and file nestings.
 
 use std::{iter::Sum, ops};
 
